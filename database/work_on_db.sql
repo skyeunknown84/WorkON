a4b80cb7aebf0fc29b6c5313e8fb683244f32bcf
skyeunknown84 <skyeunknown84@users.noreply.github.com>
2021-12-08 07:52:16 +0800

    New Database Schema
    Update Database Schema
    (pls. re-import in PhpMyAdmin)

--- database/work_on_db.sql
+++ database/work_on_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2020 at 08:37 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Dec 08, 2021 at 12:50 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,23 @@ SET time_zone = "+00:00";
 --
 -- Database: `work_on_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_list`
+--
+
+CREATE TABLE `group_list` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `group_name` text NOT NULL,
+  `member` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `member_id` int(30) NOT NULL,
+  `user_ids` text NOT NULL,
+  `date_created` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -44,8 +61,9 @@ CREATE TABLE `project_list` (
 --
 
 INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
-(1, 'Sample Project', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. In elementum, metus vitae malesuada mollis, urna nisi luctus ligula, vitae volutpat massa eros eu ligula. Nunc dui metus, iaculis id dolor non, luctus tristique libero. Aenean et sagittis sem. Nulla facilisi. Mauris at placerat augue. Nullam porttitor felis turpis, ac varius eros placerat et. Nunc ut enim scelerisque, porta lacus vitae, viverra justo. Nam mollis turpis nec dolor feugiat, sed bibendum velit placerat. Etiam in hendrerit leo. Nullam mollis lorem massa, sit amet tincidunt dolor lacinia at.&lt;/span&gt;							', 0, '2020-11-03', '2021-01-20', 2, '3,4,5', '2020-12-03 09:56:56'),
-(2, 'Sample Project 102', 'Sample Only', 0, '2020-12-02', '2020-12-31', 2, '3', '2020-12-03 13:51:54');
+(9, 'Christmas Party', '																														List anything activities and needed stuff for the Annual Christmas Party!																									', 1, '2021-12-02', '2021-12-25', 9, '6', '2021-12-02 19:13:42'),
+(10, 'Christmas Movies', '												&lt;p&gt;List All Holidays Movie Marathons (2019-2021)&lt;/p&gt;										', 1, '2021-12-04', '2021-12-04', 9, '10,12,11,6', '2021-12-02 22:10:15'),
+(11, 'Work-On: A Task Management System', 'Create A Baby Tasks', 1, '2021-12-07', '2021-12-09', 9, '10,12,11', '2021-12-07 05:33:45');
 
 -- --------------------------------------------------------
 
@@ -67,7 +85,7 @@ CREATE TABLE `system_settings` (
 --
 
 INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
-(1, 'Task Management System', 'info@sample.comm', '+6948 8542 623', '2102  Caldwell Road, Rochester, New York, 14608', '');
+(1, 'WorkON', 'info@workon.com', '+6397701234567', '1111 New Build Avenue Drive Street Working Hub City, Philippines', 'work-on-logo.png');
 
 -- --------------------------------------------------------
 
@@ -79,7 +97,9 @@ CREATE TABLE `task_list` (
   `id` int(30) NOT NULL,
   `project_id` int(30) NOT NULL,
   `task` varchar(200) NOT NULL,
+  `task_owner` varchar(255) NOT NULL,
   `description` text NOT NULL,
+  `file_uploaded` longtext NOT NULL,
   `status` tinyint(4) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -88,11 +108,12 @@ CREATE TABLE `task_list` (
 -- Dumping data for table `task_list`
 --
 
-INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `date_created`) VALUES
-(1, 1, 'Sample Task 1', '								&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-size: 14px; text-align: justify;&quot;&gt;Fusce ullamcorper mattis semper. Nunc vel risus ipsum. Sed maximus dapibus nisl non laoreet. Pellentesque quis mauris odio. Donec fermentum facilisis odio, sit amet aliquet purus scelerisque eget.&amp;nbsp;&lt;/span&gt;													', 3, '2020-12-03 11:08:58'),
-(2, 1, 'Sample Task 2', 'Sample Task 2							', 1, '2020-12-03 13:50:15'),
-(3, 2, 'Task Test', 'Sample', 1, '2020-12-03 13:52:25'),
-(4, 2, 'test 23', 'Sample test 23', 1, '2020-12-03 13:52:40');
+INSERT INTO `task_list` (`id`, `project_id`, `task`, `task_owner`, `description`, `file_uploaded`, `status`, `date_created`) VALUES
+(10, 9, 'Christmas Gifts', 'Adam', '&lt;ul&gt;&lt;li&gt;Monito -&amp;gt; Monita&lt;/li&gt;&lt;li&gt;Monita -&amp;gt; Monito&lt;/li&gt;&lt;/ul&gt;', '', 1, '2021-12-02 19:48:26'),
+(11, 10, 'Christmas Party', 'Ethan', '							', '', 1, '2021-12-02 23:29:06'),
+(12, 9, 'Christmas Dinner', 'Ellie', '							', '', 1, '2021-12-02 23:43:39'),
+(13, 11, 'Make A Responsive UI in Login and Register via Bootstrap', 'Adam', 'Tasd', '', 1, '2021-12-07 05:37:02'),
+(14, 10, 'Birthday Party', 'Ellie', '&lt;p&gt;Daddy&amp;#x2019;s Bday&lt;/p&gt;&lt;p&gt;Auntie&amp;#x2019;s Bday&lt;/p&gt;', '', 1, '2021-12-08 06:10:46');
 
 -- --------------------------------------------------------
 
@@ -116,11 +137,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
-(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 'no-image-available.png', '2020-11-26 10:57:04'),
-(2, 'John', 'Smith', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', 2, '1606978560_avatar.jpg', '2020-12-03 09:26:03'),
-(3, 'Claire', 'Blake', 'cblake@sample.com', '4744ddea876b11dcb1d169fadf494418', 3, '1606958760_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 09:26:42'),
-(4, 'George', 'Wilson', 'gwilson@sample.com', 'd40242fb23c45206fadee4e2418f274f', 3, '1606963560_avatar.jpg', '2020-12-03 10:46:41'),
-(5, 'Mike', 'Williams', 'mwilliams@sample.com', '3cc93e9a6741d8b40460457139cf8ced', 3, '1606963620_47446233-clean-noir-et-gradient-sombre-image-de-fond-abstrait-.jpg', '2020-12-03 10:47:06');
+(1, 'Administrator', '', 'admin@workon.com', '0192023a7bbd73250516f069df18b500', 1, '1638257340_rocky-sea-bg.PNG', '2020-11-26 10:57:04'),
+(6, 'Nancy', 'Drew', 'nancydrew@workon.com', 'f4f0d1165fbf5ddc5fbf4c214af9f2b2', 3, '1636958700_Screenshot (1).png', '2021-11-15 14:45:39'),
+(8, 'Jen', 'Del', 'jendel@workon.com', '0ed3d2694dbb0fb3a8e75b5b5ac80cf6', 2, '1636965480_IMG_20180825_134618.jpg', '2021-11-15 16:38:52'),
+(9, 'bea', 'gal', 'beagal@workon.com', '371b3ac948d5928e52206be791de78f3', 2, '1638429540_Ellie-7th-Months.jpg', '2021-12-02 15:19:03'),
+(10, 'Adam', 'Dom', 'adam@workon.com', '3e7b522b9756d2578d3a86d8f366be6e', 3, '1638444480_IMG_20200909_162649.jpg', '2021-12-02 19:28:04'),
+(11, 'Ethan', 'end', 'ethan@wotkon.com', '95b82c0376b58d81b9a7283be2ead91c', 3, '1638444600_Ethan-1st-BDay.png', '2021-12-02 19:30:12'),
+(12, 'Ellie', 'Pal', 'ellie@workon.com', 'ce33397f0e2fdb6e5c7e83da88bf823f', 3, '1638444660_Ellie-7th-Months.jpg', '2021-12-02 19:31:13');
 
 -- --------------------------------------------------------
 
@@ -138,23 +161,20 @@ CREATE TABLE `user_productivity` (
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
   `user_id` int(30) NOT NULL,
+  `status` tinyint(4) NOT NULL,
   `time_rendered` float NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user_productivity`
---
-
-INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
-(1, 1, 1, '							&lt;p&gt;Sample Progress&lt;/p&gt;&lt;ul&gt;&lt;li&gt;Test 1&lt;/li&gt;&lt;li&gt;Test 2&lt;/li&gt;&lt;li&gt;Test 3&lt;/li&gt;&lt;/ul&gt;																			', 'Sample Progress', '2020-12-03', '08:00:00', '10:00:00', 1, 2, '2020-12-03 12:13:28'),
-(2, 1, 1, '							Sample Progress						', 'Sample Progress 2', '2020-12-03', '13:00:00', '14:00:00', 1, 1, '2020-12-03 13:48:28'),
-(3, 1, 2, '							Sample						', 'Test', '2020-12-03', '08:00:00', '09:00:00', 5, 1, '2020-12-03 13:57:22'),
-(4, 1, 2, 'asdasdasd', 'Sample Progress', '2020-12-02', '08:00:00', '10:00:00', 2, 2, '2020-12-03 14:36:30');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `group_list`
+--
+ALTER TABLE `group_list`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `project_list`
@@ -191,10 +211,16 @@ ALTER TABLE `user_productivity`
 --
 
 --
+-- AUTO_INCREMENT for table `group_list`
+--
+ALTER TABLE `group_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `system_settings`
@@ -206,19 +232,19 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `user_productivity`
 --
 ALTER TABLE `user_productivity`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
